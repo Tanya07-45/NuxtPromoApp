a081f24463a19ef12d9f4f1d18198b91d12f6efd
Tanya Shukla <stanya97@gmail.com>
2021-10-25 16:30:37 +0530

    login and register page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
 
-   <Hero />
+   <hero />
     <section class="section">
       <div class="container">
         <h1 class="title">Featured Courses</h1>
         <div class="columns">
           <div class="column is-one-quarter">
-           <CourseCard/>
+           <course-card/>
           </div>
         </div>
       </div>
@@ -17,7 +17,7 @@
         <h1 class="title">Featured Articles</h1>
         <div class="columns">
           <div class="column is-one-quarter">
-            <BlogCard/>
+            <blog-card/>
           </div>
         </div>
       </div>
