1c64d66fd77534c7b4171fb33c2e0dbfa0ffaf29
Tanya Shukla <stanya97@gmail.com>
2021-10-26 01:02:26 +0530

    provide courses to card

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-
    <hero />
     <section class="section">
       <div class="container">
         <h1 class="title">Featured Courses</h1>
         <div class="columns">
-          <div class="column is-one-quarter">
-           <course-card/>
+          <!-- itirate coloumn with v-for and dont forget  :key -->
+       <div
+            v-for="course in courses"
+            :key="course._id"
+            class="column is-one-quarter">
+            <!-- pass a course as a prop to course-card -->
+            <course-card :course="course"/>
           </div>
         </div>
       </div>
