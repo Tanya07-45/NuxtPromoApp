7bd0c557bc037b0776a1ac3dacabd8b050f55e27
Tanya Shukla <stanya97@gmail.com>
2021-10-25 14:35:29 +0530

    components of app

---
+++ components/CourseCard.vue
@@ -0,0 +1,48 @@
+<template>
+     <div class="card">
+       <div class="card-image">
+            <figure class="image is-4by2">
+              <img src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="Placeholder image">
+             </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Some Super Title</p>
+                <p class="subtitle is-6"><i>Some Super Subtitle</i></p>
+            </div>
+        </div>
+                <div class="content">
+                  Some Description
+                  <br>
+                </div>
+                <div class="price-box">
+                  <span class="price">150$</span>
+                  <span class="disc-price">9.99$</span>
+                </div>
+              </div>
+              <footer class="card-footer">
+                <nuxt-link :to="''" class="card-footer-item">Learn More</nuxt-link>
+                <a target="_" :href="'#'" class="card-footer-item">Enroll</a>
+              </footer>
+            </div> 
+</template>
+<style scoped lang="scss">
+  .card-image:hover {
+    cursor: pointer;
+    opacity: 0.9;
+  }
+  .price-box {
+    text-align: right;
+    .price {
+      color: gray;
+      font-size: 16px;
+      text-decoration: line-through;
+    }
+    .disc-price {
+      font-size: 21px;
+      font-weight: bold;
+    }
+  }
+  // card item end
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-promo-template
-      </h1>
-      <h2 class="subtitle">
-        My super Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+
+   <Hero />
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Featured Courses</h1>
+        <div class="columns">
+          <div class="column is-one-quarter">
+           <CourseCard/>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Featured Articles</h1>
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <BlogCard/>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import  CourseCard  from '~/components/CourseCard'
+import  BlogCard  from '~/components/BlogCard'
+import  Hero  from '~/components/shared/Hero'
 export default {
   components: {
-    Logo
+    CourseCard,BlogCard,Hero
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped lang="scss">
+  
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  
 
-.links {
-  padding-top: 15px;
-}
+  // Home page
+  .links {
+    padding-top: 15px;
+  }
 </style>
