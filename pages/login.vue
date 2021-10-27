9d5f79ac0403f1e8a787f244c3dd7ac45548ea6c
Tanya Shukla <stanya97@gmail.com>
2021-10-27 14:18:12 +0530

    validate inputs

--- pages/login.vue
+++ pages/login.vue
@@ -12,32 +12,35 @@
             <form>
               <div class="field">
                 <div class="control">
-                  <input class="input is-large"
+                  <input 
+                         v-model="form.email"
+                         class="input is-large"
                          type="email"
                          placeholder="Your Email"
                          autofocus=""
                          autocomplete="email">
-                  <!-- <div class="form-error">
-                    <span class="help is-danger">Email is required</span>
-                    <span class="help is-danger">Email address is not valid</span>
-                  </div> -->
+                  <div v-if="$v.form.email.$error" class="form-error">
+                    <span v-if="!$v.form.email.required" class="help is-danger">Email is required</span>
+                    <span v-if="!$v.form.email.emailValidator" class="help is-danger">Email address is not valid</span>
+                  </div>
                 </div>
               </div>
               <div class="field">
                 <div class="control">
                   <input
+                    v-model="form.password"
                     class="input is-large"
                     type="password"
                     placeholder="Your Password"
                     autocomplete="current-password">
-                  <!-- <div class="form-error">
-                    <span class="help is-danger">Password is required</span>
-                  </div> -->
+                  <div  v-if="$v.form.password.$error" class="form-error">
+                    <span v-if="!$v.form.password.required" class="help is-danger">Password is required</span>
+                  </div>
                 </div>
               </div>
               <!-- Login Button -->
               <button
-                @click.prevent="() => {}"
+                @click.prevent="login"
                 class="button is-block is-info is-large is-fullwidth">
                 Login
               </button>
@@ -54,7 +57,35 @@
     </div>
   </section>
 </template>
-
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+export default {
+  data() {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    }
+  },
+  validations: {
+    form: {
+      email: {
+        emailValidator:email ,
+        required
+      },
+      password: {
+        required
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$v.form.$touch()//validate the input
+    }
+  }
+}
+</script>
 <style scoped>
   .hero.is-success {
     background: #F2F6FA;
